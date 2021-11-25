68be1a012722731553acd59590f8c9f7900a1c5e
Carolyn Grady <owenv8@gmail.com>
2021-11-24 16:58:11 -0800

    Update and rename DeleteContact.php to DeleteOrder.php

--- API/DeleteOrder.php
+++ API/DeleteOrder.php
@@ -5,8 +5,8 @@
 
 	$inData = getRequestInfo();
 
-	$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserID=? AND ContactID=?");
-	$stmt->bind_param("ss", $inData["UserID"], $inData["ContactID"]);
+	$stmt = $conn->prepare("DELETE FROM order WHERE fid=? AND cid=? AND isbn=? AND semester=?");
+	$stmt->bind_param("iiis", $inData["fid"], $inData["cid"], $inData["isbn"], $inData["semester"]);
 	$stmt->execute();
 
 	returnWithInfo($stmt->affected_rows);
