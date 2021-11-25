800d39f09a7277bdbf10f43f624a4d450469d3b5
Carolyn Grady <owenv8@gmail.com>
2021-11-24 16:55:22 -0800

    Update and rename AddContact.php to NewOrder.php

---
+++ API/NewOrder.php
@@ -0,0 +1,30 @@
+<?php
+	// include database connection file
+	include "dbConfig.php";
+	include "returnFunctions.php";
+
+	$inData = getRequestInfo();
+	
+	// check if order for this class from this teacher for this book already exists for this semester/year
+	$stmt = $conn->prepare("SELECT * FROM orders WHERE fid=? AND cid=? AND isbn=? AND semester=?");
+	$stmt->bind_param("iii", $inData["fid"], $inData["cid"], $inData["isbn"], $inData["semester"]);
+	$stmt->execute();
+
+	$result = $stmt->get_result();
+	
+	// if the order exists return error
+	// else insert the order to db
+	if ($result->num_rows > 0){
+		returnWithError("Order Already Exists");
+	}
+	else{
+		$stmt = $conn->prepare("INSERT INTO orders (fid, cid, isbn, orderBy) VALUES (?, ?, ?, ?)");
+		$stmt->bind_param("iiids", $inData["fid"], $inData["cid"], $inData["isbn"], $inData["orderBy"], $inData["semester"]);
+		$stmt->execute();
+
+		returnWithInfo($stmt->affected_rows);
+	}
+	
+	$stmt->close();
+	$conn->close();
+?>
