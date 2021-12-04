e7e935304fc8d2f7fa52ef7552330e745fc477e7
ericsayegh <ericsayegh@knights.ucf.edu>
2021-12-04 15:04:32 -0500

    Added a few stuff to admin page

--- js/adminMain.js
+++ js/adminMain.js
@@ -1,2 +1,21 @@
 
+function showChangePasswordContent()
+{
+    $("#createAdminContent").hide();
+    $("#viewRequestsContent").hide();
+    $("#changePasswordContent").toggle();
+}
 
+function showCreateAdminContent()
+{
+    $("#changePasswordContent").hide();
+    $("#viewRequestsContent").hide();
+    $("#createAdminContent").toggle();
+}
+
+function showBookRequestContent()
+{
+    $("#changePasswordContent").hide();
+    $("#createAdminContent").hide();
+    $("#viewRequestsContent").toggle();
+}
