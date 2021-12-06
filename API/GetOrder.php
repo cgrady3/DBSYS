b0a7c504141ec506f3c0910c7b6b8cfc8dba7a83
ericsayegh <ericsayegh@knights.ucf.edu>
2021-12-05 23:13:12 -0500

    Update GetOrder.php

--- API/GetOrder.php
+++ API/GetOrder.php
@@ -1,7 +1,10 @@
 <?php
 	// include database connection file
-	include "dbConfig.php";
+	//include "dbConfig.php";
+	//include "returnFunctions.php";
+	include "A2HostingDBConfig.php";
 	include "returnFunctions.php";
+
 
 	$inData = getRequestInfo();
 
