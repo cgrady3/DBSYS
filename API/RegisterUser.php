7fb4e880b14e4d322b6cdbeb8dbaf63b5526f332
Max Gomer <maximilliangomer@gmail.com>
2021-12-06 11:37:49 -0500

    Test, comment out select in registeruser

--- API/RegisterUser.php
+++ API/RegisterUser.php
@@ -23,9 +23,9 @@
 		$stmt->bind_param("ssss", $inData["name"], $inData["email"], $inData["password"], $inData["isStaff"]);
 		$stmt->execute();
 
-		$stmt = $conn->prepare("SELECT * FROM faculty WHERE email=?");
-		$stmt->bind_param("s", $inData["email"]);
-		$stmt->execute();
+		// $stmt = $conn->prepare("SELECT * FROM faculty WHERE email=?");
+		// $stmt->bind_param("s", $inData["email"]);
+		// $stmt->execute();
 
 		$result = $stmt->get_result();
 		echo json_encode($result->fetch_assoc());
