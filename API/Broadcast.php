768af62f6a1d5120906a32c33d8f8bec985bd4c7
ericsayegh <ericsayegh@knights.ucf.edu>
2021-12-06 19:25:32 -0500

    Update Broadcast.php

--- API/Broadcast.php
+++ API/Broadcast.php
@@ -15,11 +15,11 @@
   	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()){
 			$rows[] = $row['email'];
-			$singleEmail = ','.$row['email'][0]
-			$emailList .= $singleEmail
+			//$singleEmail = ','.$row['email']
+			//$emailList .= $singleEmail
 
 		}
-		echo json_encode($emailList);
+		echo json_encode($rows[0]);
   	}
     	else {
 		returnWithError("No emails Found");
