a5bf3eb882f1a8dcf5da5b37c20da1d8b70237ca
Carolyn Grady <owenv8@gmail.com>
2021-12-06 20:42:47 -0500

    lp

--- js/main.js
+++ js/main.js
@@ -214,7 +214,7 @@ $(".semester").click((e) => {
 
   $("#viewRequestsContent").hide();
 
-  currSemester = $(this).text;
+  currSemester = $(this).text();
   console.log("currsememster: " + currSemester);
 
   if (isStaff) {
