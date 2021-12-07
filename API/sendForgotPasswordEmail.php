c50b5d237b695d1864ab7a0ad336005db5db8654
Max Gomer <maximilliangomer@gmail.com>
2021-12-07 09:05:59 -0500

    Update sendForgotPasswordEmail.php

--- API/sendForgotPasswordEmail.php
+++ API/sendForgotPasswordEmail.php
@@ -9,11 +9,11 @@ ini_set('display_errors', 'on');
 
 $inData = getRequestInfo();
 
-	$to			= $inData["email"];
-#	$to			= 'maximilliangomer@gmail.com';
+#	$to			= $inData["email"];
+	$to			= 'maximilliangomer@gmail.com';
 	$subject	= 'Reset Password';
-#	$message 	= $inData["message"];
-	$message 	= 'test message!';
+	$message 	= $inData["message"];
+#	$message 	= 'test message!';
 	$headers	= 'From: group25@databases.com'			. "\r\n" .
 					'Reply-To: group25@databases.com'	. "\r\n" .
 					'X-Mailer: PHP/' . phpversion();
